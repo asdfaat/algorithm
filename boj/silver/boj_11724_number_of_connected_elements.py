d8e34889fe1b7ed039eb0415d06a15a34b68fca8
Bang <51738138+asdfaat@users.noreply.github.com>
2025-10-01 15:33:45 +0900

    Update boj_11724_number_of_connected_elements.py

--- boj/silver/boj_11724_number_of_connected_elements.py
+++ boj/silver/boj_11724_number_of_connected_elements.py
@@ -1,6 +1,6 @@
-# faild: 시간 초과
-import sys
 
+import sys
+from collections import deque
 read = sys.stdin.readline
 
 # n: 정점의 개수, m: 간선의 개수
@@ -16,21 +16,20 @@ for _ in range(m):
 
 # search
 cnt = 0  # 연결 요소의 개수
-visited = set() # <- 이거보다 리스트 인덱스 접근이 빠름
+visited = [False] * (n+1)
 
-for i in range(1,n): # <- 마지막 노드 안 돌았음
-    if i in visited:
+for i in range(1,n+1): 
+    if visited[i]:
         continue
     
-    queue = [i]
+    queue = deque([i])
     while queue:
-        u = queue.pop(0) # <- 이게 O(n)의 시간 복잡도
-        visited.add(u)
-
+        u = queue.popleft()
+        
         for v in edges[u]:
-            if v not in visited:
+            if not visited:
                 queue.append(v)
-    
+                visited[v] = True
     cnt += 1
 
 print(cnt)
